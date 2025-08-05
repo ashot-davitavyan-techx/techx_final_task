306254909a588d746bad285968a95e61b2e036e5
Ashot Davitavyan <ashot.davitavyan.techx@gmail.com>
2025-08-05 23:30:40 +0400

    finished header and footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', function(){
+	const currentScroll = this.window.scrollY;
+	const searchIcon = document.getElementById("search-icon");
+	const header = this.document.getElementById("header");
+	const hideClass = searchIcon.classList.contains("active") ? "hide-searchbar" : "hide-no-searchbar";
+	if (currentScroll > lastScrollY && currentScroll > 50) {
+		header.classList.add(hideClass);
+	} else {
+		header.classList.remove(hideClass);
+	}
+	lastScrollY = currentScroll;
+});
+
 document.addEventListener("click", function(event){
 	const popup = document.getElementById("login-dropdown");
 	const popupCaret = document.getElementById("login-dropdown-caret");
@@ -14,12 +29,21 @@ document.getElementById("login-dropdown-button").addEventListener("click", funct
 });
 
 document.getElementById("menu-button").addEventListener("click", function(){
-	
+	sidebar = document.getElementById("side-menu");
+	sidebar.classList.toggle("menu-active");
+});
+
+document.querySelectorAll(".menu-toggle-button").forEach(button => {
+		button.addEventListener("click", function(){
+			const subMenu = button.closest('li').querySelector('.sub-menu');
+			subMenu.classList.toggle('open');
+	});
 });
 
 document.getElementById("search-button").addEventListener("click", function(){
 	const searchIcon = document.getElementById("search-icon");
 	const searchbar = document.getElementById("searchbar");
+	const hideProperty = document.querySelector(".hide");
 
 	if (!searchIcon.classList.contains("active")){
 		searchIcon.src = "../media/x.svg";
@@ -32,5 +56,4 @@ document.getElementById("search-button").addEventListener("click", function(){
 		searchIcon.classList.remove("active");
 		searchbar.style.display = "none";
 	}
-
 });
